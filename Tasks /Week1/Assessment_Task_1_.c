262c977d774c37e97021d489da98e1f997078ec6
DafneRibeiro <dafsru@icloud.com>
2025-07-20 19:39:50 +0100

    Assessment Task 1

---
+++ Tasks /Week1/Assessment_Task_1_.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+  // Marks between 40-49
+
+  // this program calculates the total hours spent on different subjects and prints the results
+
+  // I initialed the variables for the hours spent on each subject
+  int math_hours;
+  int programming_hours;
+  int networking_hours;
+
+  // and assigned them values
+  math_hours = 10;
+  programming_hours = 30;
+  networking_hours = 25;
+
+  // I used printf to display the total hours spent on each subject
+  printf("Total hours spent on each subject:\n");
+  printf("Math hours: %d\n", math_hours);
+  printf("Programming hours: %d\n", programming_hours);
+  printf("Networking hours: %d\n", networking_hours);
+}
